77e075b8ad9b5b8d9d74a62094602db58072c7a5
Enzo Amaral <enzoamaral2006@gmail.com>
2026-09-16 09:05:26 -0300

    Initial commit

---
+++ app/src/main/java/com/ucsal/atividadeavaliativa/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ucsal.atividadeavaliativa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonConfig = findViewById(R.id.button_configurar);
+        Button buttonSair = findViewById(R.id.button_sair);
+        buttonConfig.setOnClickListener(this);
+        buttonSair.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.button_configurar) {
+            Intent i = new Intent(this, ConfiguracaoActivity.class);
+            startActivity(i);
+        }
+
+        if (id == R.id.button_sair) {
+            finish();
+        }
+    }
+}
